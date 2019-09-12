3d2b5ae08429de544ad45603856bb3ea287849b7
Adityaraut18 <51366486+Adityaraut18@users.noreply.github.com>
2019-09-12 23:50:09 +0530

    Add files via upload

---
+++ minor/routes.py
@@ -0,0 +1,26 @@
+from flask import render_template, url_for, flash, redirect, request
+from minor.models import User
+from minor import app,db,bcrypt
+from minor.forms import RegistrationForm
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
